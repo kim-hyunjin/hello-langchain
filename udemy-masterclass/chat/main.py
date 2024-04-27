c12c3d23c3efe03456234c783918c60c9f0931a0
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-27 14:10:37 +0900

    use chat style model

---
+++ udemy-masterclass/chat/main.py
@@ -0,0 +1,36 @@
+from langchain.prompts import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(
+    memory_key="messages",
+    return_messages=True,
+    chat_memory=FileChatMessageHistory("messages.json"),
+)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
+)
+
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+
+while True:
+    content = input(">> ")
+
+    result = chain({"content": content})
+
+    print(f"Result: {result}")
