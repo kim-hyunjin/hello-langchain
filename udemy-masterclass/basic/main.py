67d8d07f109dab7bb7077c709cdc8d590a8e0d0e
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-27 13:32:17 +0900

    lint 적용

--- udemy-masterclass/basic/main.py
+++ udemy-masterclass/basic/main.py
@@ -16,34 +16,23 @@ llm = OpenAI()
 
 code_prompt = PromptTemplate(
     template="Write a very short {language} function that will {task}. without explanation.",
-    input_variables=["language", "task"]
+    input_variables=["language", "task"],
 )
 test_prompt = PromptTemplate(
     template="Write a test for the following {language} code without explanation:\n{code}",
-    input_variables=["language", "code"]
+    input_variables=["language", "code"],
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt,
-    output_key="code"
-)
-test_chain = LLMChain(
-    llm=llm,
-    prompt=test_prompt,
-    output_key="test"
-)
+code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
+test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
 
 chain = SequentialChain(
     chains=[code_chain, test_chain],
     input_variables=["task", "language"],
-    output_variables=["code", "test"]
+    output_variables=["code", "test"],
 )
 
-result = chain({
-    "language": args.language,
-    "task": args.task
-})
+result = chain({"language": args.language, "task": args.task})
 
 print(">>>>>> generated code:")
 print(result["code"])
