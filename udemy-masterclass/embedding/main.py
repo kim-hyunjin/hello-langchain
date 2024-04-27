6629f2a7f8037c124b3a0cbaafcd9e96090148ac
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-27 14:54:29 +0900

    chunk texts and calculate embeddings

---
+++ udemy-masterclass/embedding/main.py
@@ -0,0 +1,17 @@
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+"""
+emb = embeddings.embed_query("hi there")
+print(emb)
+"""
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(text_splitter=text_splitter)
+
+print(docs)
