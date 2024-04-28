ea091b4090377d4fac1280f4d10398d5d405fe74
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-28 13:40:54 +0900

    using chroma for vector store

--- udemy-masterclass/embedding/main.py
+++ udemy-masterclass/embedding/main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,4 +15,15 @@ text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_over
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
-print(docs)
+# instantly, calculate embeddings with openai - it cost fraction of penny
+db = Chroma.from_documents(
+    docs, embedding=embeddings, persist_directory="emb", k=1
+)  # k mean count of getting relavant item
+
+results = db.similarity_search_with_score(
+    "what is interesting facts of English language?"
+)
+
+for result in results:
+    print("\n")
+    print(result)
