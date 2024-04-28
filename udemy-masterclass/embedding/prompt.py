a1d398a5587a3e39b9a178ca58c5a00b52c2e3fb
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-28 16:42:08 +0900

    custom retriever - remove duplicated documents

--- udemy-masterclass/embedding/prompt.py
+++ udemy-masterclass/embedding/prompt.py
@@ -3,13 +3,14 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 
 load_dotenv()
 
 chat = ChatOpenAI()
 embeddings = OpenAIEmbeddings()
 db = Chroma(persist_directory="emb", embedding_function=embeddings)
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db)
 """
 retriever는 다양한 db를 지원하기 위한 인터페이스
 문자열을 받아 관련있는 도큐먼트들을 리턴하는 get_relevant_documents 함수가 있음
