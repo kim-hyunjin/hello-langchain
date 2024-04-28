a08233207b8c0d5c916146be331ab702a729e6d8
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-28 14:13:41 +0900

    retriever 주석 추가

--- udemy-masterclass/embedding/prompt.py
+++ udemy-masterclass/embedding/prompt.py
@@ -10,8 +10,16 @@ chat = ChatOpenAI()
 embeddings = OpenAIEmbeddings()
 db = Chroma(persist_directory="emb", embedding_function=embeddings)
 retriever = db.as_retriever()
+"""
+retriever는 다양한 db를 지원하기 위한 인터페이스
+문자열을 받아 관련있는 도큐먼트들을 리턴하는 get_relevant_documents 함수가 있음
+"""
 
-chain = RetrievalQA.from_chain_type(llm=chat, retriever=retriever, chain_type="stuff")
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff",  # take some context from the vector store and stuff it into prompt(SystemMessagePromptTemplate)
+)
 
 result = chain.run("what is interesting facts about the English language?")
 print(result)
