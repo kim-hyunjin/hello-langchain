93464637f83186d03613d5c57f41620b363956c8
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-30 23:17:14 +0900

    코멘트 추가

--- udemy-masterclass/agents/main.py
+++ udemy-masterclass/agents/main.py
@@ -19,7 +19,18 @@ prompt = ChatPromptTemplate(
     ]
 )
 tools = [run_query_tool]
+"""
+Agent
+- A chain that knows how to use tools
+- Will take that list of tools and convert them into JSON function descriptions
+- Still has input variables, memory, prompts, etc - all the normal things a chain has
+"""
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
+"""
+Agent Executor
+- Takes an agent and runs it until the response is not a function call
+- Essentially a function while loop
+"""
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
 agent_executor("How many users are in the database?")
