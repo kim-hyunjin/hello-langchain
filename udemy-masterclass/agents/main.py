d427c2914e62c15072673daa9bf00be0b44d1b92
kim-hyunjin <hyunjin1612@gmail.com>
2024-05-01 00:02:55 +0900

    시스템 메시지, 테이블 설명 Tool 추가

--- udemy-masterclass/agents/main.py
+++ udemy-masterclass/agents/main.py
@@ -4,21 +4,26 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tools
 
 load_dotenv()
 
 chat = ChatOpenAI()
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to a SQLite database.\n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tools]
 """
 Agent
 - A chain that knows how to use tools
@@ -33,4 +38,4 @@ Agent Executor
 """
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have provided a shipping address?")
