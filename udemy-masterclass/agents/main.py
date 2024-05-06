5f745e771e9a2c0e2d67ad6aabf71205b46c1601
kim-hyunjin <hyunjin1612@gmail.com>
2024-05-06 13:15:42 +0900

    report tool 추가

--- udemy-masterclass/agents/main.py
+++ udemy-masterclass/agents/main.py
@@ -9,6 +9,7 @@ from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tools
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -28,7 +29,7 @@ prompt = ChatPromptTemplate(
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
-tools = [run_query_tool, describe_tables_tools]
+tools = [run_query_tool, describe_tables_tools, write_report_tool]
 """
 Agent
 - A chain that knows how to use tools
@@ -43,4 +44,7 @@ Agent Executor
 """
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
-agent_executor("How many users have provided a shipping address?")
+# agent_executor("How many users have provided a shipping address?")
+agent_executor(
+    "Summarize the top 5 most popular products. Write the results to a report file."
+)
