1533ea2547ddb55510be05f994b7e2c3c61272d6
kim-hyunjin <hyunjin1612@gmail.com>
2024-06-07 18:30:52 +0900

    vector store로 pinecone 사용하기

--- udemy-masterclass/pdf-app/app/chat/create_embeddings.py
+++ udemy-masterclass/pdf-app/app/chat/create_embeddings.py
@@ -1,5 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from app.chat.vector_stores.pinecone import vector_store
 
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
@@ -24,4 +25,4 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
 
-    print(docs)
+    vector_store.add_documents(docs)
