3e796eafd8876d954a4ff06d0a8e0a9d613ab6ba
kim-hyunjin <hyunjin1612@gmail.com>
2024-06-16 15:50:47 +0900

    임베딩 생성 시 메타데이터 수정

--- udemy-masterclass/pdf-app/app/chat/create_embeddings.py
+++ udemy-masterclass/pdf-app/app/chat/create_embeddings.py
@@ -25,5 +25,12 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
 
+    for doc in docs:
+        doc.metadata = {
+            "page": doc.metadata["page"],
+            "text": doc.page_content,
+            "pdf_id": pdf_id,
+        }
+
     # 아래에서 임베딩을 만드는 데 오랜 시간이 걸림
     vector_store.add_documents(docs)
