edf207cdb44f6de599e0a6e4906628951acac2ad
kim-hyunjin <hyunjin1612@gmail.com>
2024-06-29 22:38:30 +0900

    ConversationalRetrievalChain

--- udemy-masterclass/pdf-app/app/chat/chat.py
+++ udemy-masterclass/pdf-app/app/chat/chat.py
@@ -1,5 +1,8 @@
+from langchain.chains import ConversationalRetrievalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.llms.chatopenai import build_llm
+from app.chat.memories.sql_memory import build_memory
 
 
 def build_chat(chat_args: ChatArgs):
@@ -15,3 +18,9 @@ def build_chat(chat_args: ChatArgs):
     """
 
     retriever = build_retriever(chat_args=chat_args)
+    llm = build_llm(chat_args=chat_args)
+    memory = build_memory(chat_args=chat_args)
+
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm, memory=memory, retriever=retriever
+    )
