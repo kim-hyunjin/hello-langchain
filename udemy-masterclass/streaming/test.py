a0c174aa8578dd4c704fce33f51e04389a0f3dd1
kim-hyunjin <hyunjin1612@gmail.com>
2024-07-14 13:28:27 +0900

    chat streaming mode

---
+++ udemy-masterclass/streaming/test.py
@@ -0,0 +1,13 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI(streaming=True)
+
+prompt = ChatPromptTemplate.from_messages([("human", "{content}")])
+messages = prompt.format_messages(content="tell me a joke")
+
+for message in chat.stream(messages):
+    print(message)
