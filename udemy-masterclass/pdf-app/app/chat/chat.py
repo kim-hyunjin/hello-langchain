b39eef5303067091fff6b7c54d3cfc3b0456dfd2
kim-hyunjin <hyunjin1612@gmail.com>
2024-07-14 17:22:21 +0900

    스트리밍 가능 적용 완료

--- udemy-masterclass/pdf-app/app/chat/chat.py
+++ udemy-masterclass/pdf-app/app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -19,8 +20,16 @@ def build_chat(chat_args: ChatArgs):
 
     retriever = build_retriever(chat_args=chat_args)
     llm = build_llm(chat_args=chat_args)
+
+    # 대화 history와 현재 유저 질문을 요약해서 다른 모델로 넘길 때(condense_question_llm일 때)는
+    # 스트리밍을 종료시키지 않도록 아래와 같이 별도의 llm을 만들어 사용
+    # => 뒤에 오는 llm에서도 스트리밍을 사용해야하므로 앞에서 종료시키면 안됨
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args=chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
-        llm=llm, memory=memory, retriever=retriever
+        llm=llm,
+        condense_question_llm=condense_question_llm,
+        memory=memory,
+        retriever=retriever,
     )
