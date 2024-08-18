6b62d87adc0e305241ea4af49cd45e5e436a5ff5
kim-hyunjin <hyunjin1612@gmail.com>
2024-08-18 19:28:58 +0900

    에이전트가 찾은 linkedin url로 데이터 스크래핑하기

--- ice-breaker/ice_breaker.py
+++ ice-breaker/ice_breaker.py
@@ -1,33 +1,35 @@
+from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_community.chat_models import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
-
-from dotenv import load_dotenv
-import os
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
+
+def ice_break_with(name: str) -> str:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_profile_url, mock=True
+    )
+
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    summary_template = """
-        given the information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables="information", template=summary_template
-    )
-
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="llama3.1")
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-macro", mock=True
-    )
-    res = chain.invoke(input={"information": linkedin_data})
-
-    print(res)
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Macro Udemy")
