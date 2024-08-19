379a68ad32a242c78ffe1527c96e1e4a74dbe9ba
kim-hyunjin <hyunjin1612@gmail.com>
2024-08-19 23:23:37 +0900

    fullstack app with flask

--- ice-breaker/ice_breaker.py
+++ ice-breaker/ice_breaker.py
@@ -1,13 +1,14 @@
+from typing import Tuple
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from parser.output_parser import summary_parser
+from parser.output_parser import Summary, summary_parser
 
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_profile_url, mock=True
@@ -29,13 +30,13 @@ def ice_break_with(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res: Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Udemy instructor Eden Macro")
+    print(ice_break_with(name="Udemy instructor Eden Macro"))
