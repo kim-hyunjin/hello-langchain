9e9b7fd8864f1bb75c627ae59b25de863246f937
kim-hyunjin <hyunjin1612@gmail.com>
2024-08-19 22:05:12 +0900

    output parser

--- ice-breaker/ice_breaker.py
+++ ice-breaker/ice_breaker.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from parser.output_parser import summary_parser
 
 
 def ice_break_with(name: str) -> str:
@@ -16,12 +17,17 @@ def ice_break_with(name: str) -> str:
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
+    \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
@@ -32,4 +38,4 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Eden Macro Udemy")
+    ice_break_with(name="Udemy instructor Eden Macro")
