a1617eed794d4425ccaac9672fb9b011ff04fff9
kim-hyunjin <hyunjin1612@gmail.com>
2024-08-21 23:34:48 +0900

    callback 추가

--- react-langchain/main.py
+++ react-langchain/main.py
@@ -9,6 +9,8 @@ from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain.schema import AgentAction, AgentFinish
 from langchain.agents.format_scratchpad import format_log_to_str
 
+from callbacks import AgentCallbackHandler
+
 load_dotenv()
 
 
@@ -36,7 +38,7 @@ if __name__ == "__main__":
 
     # hwchase17/react PromptTemplate
     template = """
-     Answer the following questions as best you can. You have access to the following tools:
+    Answer the following questions as best you can. You have access to the following tools:
 
     {tools}
 
@@ -62,7 +64,11 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools]),
     )
 
-    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    llm = ChatOpenAI(
+        temperature=0,
+        stop=["\nObservation", "Observation"],
+        callbacks=[AgentCallbackHandler()],
+    )
     agent = (
         {
             "input": lambda x: x["input"],
@@ -75,15 +81,15 @@ if __name__ == "__main__":
 
     intermediate_steps = []
 
-    while True:
-        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    agent_step: Union[AgentAction, AgentFinish] = None
+
+    while not isinstance(agent_step, AgentFinish):
+        agent_step = agent.invoke(
             {
                 "input": "What is the length of the word: DOG",
                 "agent_scratchpad": intermediate_steps,
             }
         )
-
-        print(agent_step)
 
         if isinstance(agent_step, AgentAction):
             tool_name = agent_step.tool
@@ -91,10 +97,4 @@ if __name__ == "__main__":
             tool_input = agent_step.tool_input
 
             observation = tool_to_use.func(str(tool_input))
-            print(f"{observation=}")
             intermediate_steps.append((agent_step, str(observation)))
-
-        if isinstance(agent_step, AgentFinish):
-            print("### AgentFinish ###")
-            print(agent_step.return_values)
-            break
